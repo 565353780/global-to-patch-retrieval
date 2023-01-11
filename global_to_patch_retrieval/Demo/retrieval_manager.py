5d7913f470775dfec7e4ea3f1bb5048ad3e3c489
chLi <565353780@qq.com>
2023-01-12 03:26:43 +0800

    add render control

--- global_to_patch_retrieval/Demo/retrieval_manager.py
+++ global_to_patch_retrieval/Demo/retrieval_manager.py
@@ -14,6 +14,7 @@ def demo():
     shapenet_dataset_folder_path = "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/"
     shapenet_feature_folder_path = "/home/chli/chLi/ShapeNet/features/"
     obb_info_folder_path = "/home/chli/chLi/auto-scan2cad/1314/obb_info/"
+    render = False
     print_progress = True
 
     retrieval_manager = RetrievalManager(shapenet_dataset_folder_path,
@@ -24,5 +25,5 @@ def demo():
 
     retrieval_manager.generateRetrievalResult(obb_info_folder_path,
                                               shapenet_feature_folder_path,
-                                              print_progress)
+                                              render, print_progress)
     return True
