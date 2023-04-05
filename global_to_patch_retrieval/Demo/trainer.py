3d628811ff6e8183edac5ad4d44a4d2f075266a8
chLi <565353780@qq.com>
2023-04-05 16:33:17 +0800

    refine format

--- global_to_patch_retrieval/Demo/trainer.py
+++ global_to_patch_retrieval/Demo/trainer.py
@@ -5,7 +5,7 @@ from global_to_patch_retrieval.Module.trainer import Trainer
 
 
 def demo():
-    model_file_path = "./output/pretrained_retrieval/model_best.pth"
+    model_file_path = './output/pretrained_retrieval/model_best.pth'
     resume_model_only = True
     print_progress = True
 
