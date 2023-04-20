9c283d646c91cd46dba0428d6099b8d84c32ec3f
chLi <565353780@qq.com>
2023-04-20 19:10:31 +0800

    update pytorch and cuda version

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,5 +1,4 @@
 pip install open3d numpy tqdm
 
-pip install torch torchvision torchaudio \
-  --extra-index-url https://download.pytorch.org/whl/cu116
-
+pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 \
+  --extra-index-url https://download.pytorch.org/whl/cu117
